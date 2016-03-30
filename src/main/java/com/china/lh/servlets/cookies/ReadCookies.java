4e37f59516e54e180b2564e713c2cce9444a2342
lh <jyfc7879@gmail.com>
2016-03-30 13:40:31 +0800

    add cookie Exercise

--- src/main/java/com/china/lh/servlets/cookies/ReadCookies.java
+++ src/main/java/com/china/lh/servlets/cookies/ReadCookies.java
@@ -1,7 +1,48 @@
 package com.china.lh.servlets.cookies;
 
+//imprt required java libraies
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Created by lh on 16-3-29.
  */
-public class ReadCookies {
+public class ReadCookies extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Cookie cookie = null;
+        Cookie[] cookies = null;
+        //Get an array of Cookies associated with this domain
+        cookies = request.getCookies();
+        response.setContentType("text/html");
+            //Set response content type
+        PrintWriter out = response.getWriter();
+        String title = "Reading Cookies Example";
+        String docType =
+                "<!doctype html public \"-//w3c//dtd html 4.0 " +
+                        "transitional//en\">\n";
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor=\"#f0f0f0\">\n" );
+        if(cookies != null) {
+            out.println("<h2> Found Cookies Name and Value</h2>");
+            for(int i =0;i<cookies.length;i++) {
+                cookie = cookies[i];
+                out.print("Name : " + cookie.getName( ) + ",  ");
+                out.print("Value: " + cookie.getValue( )+" <br/>");
+            }
+        } else {
+            out.println("<h2>No cookies founds</h2>");
+        }
+        out.println("</body>");
+        out.println("</html>");
+
+    }
+
+
 }
