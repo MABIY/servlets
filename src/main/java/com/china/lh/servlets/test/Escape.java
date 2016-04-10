d69a9fb0252ca05df0094392afe94a6d6b9362bc
刘华 <lh@yuan-mai.com>
2016-04-10 19:51:07 +0800

    正则表达式

--- src/main/java/com/china/lh/servlets/test/Escape.java
+++ src/main/java/com/china/lh/servlets/test/Escape.java
@@ -4,4 +4,12 @@ package com.china.lh.servlets.test;
  * Created by lh on 16-4-10.
  */
 public class Escape {
+    public static void main(String[] args) {
+        String sName = "Java转义字符(补遗)";
+
+        // reference  http://www.runoob.com/java/java-regular-expressions.html
+        sName = sName.replaceFirst("\\(补遗\\)","");
+        System.out.println(sName);
+        System.out.println(sName);
+    }
 }
