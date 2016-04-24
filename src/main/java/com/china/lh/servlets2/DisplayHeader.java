be56715fc9ede4fc479c19854a4bddddc7e908db
lh <jyfc7879@gmail.com>
2016-04-24 11:44:38 +0800

    复习一下。

---
+++ src/main/java/com/china/lh/servlets2/DisplayHeader.java
@@ -0,0 +1,7 @@
+package com.china.lh.servlets2;
+
+/**
+ * Created by lh on 16-4-23.
+ */
+public class DisplayHeader {
+}
---
+++ src/main/java/com/china/lh/servlets2/filter/autoFilter.java
@@ -0,0 +1,10 @@
+package com.china.lh.servlets2.filter;
+
+//import required libraries
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by lh on 16-4-23.
+ */
+public class autoFilter extends HttpServlet{
+}
